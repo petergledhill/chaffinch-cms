50a13f3027d7ff0206ba05b4b9ff4e85c05e6c26
Peter Gledhill <petergledhill@gmail.com>
2017-08-16 08:02:01 +0100

    initial project structure - dotnet corev1.1.2

---
+++ dotnet/src/Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chaffinch.Core
+{
+    public static class Class1
+    {
+        public static string Test()
+        {
+            return "BOB3";
+        }
+    }
+}
---
+++ dotnet/tests/Core.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Chaffinch.Core.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("BOB2",Class1.Test());            
+        }
+    }
+}
