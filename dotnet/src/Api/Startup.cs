90fe7a21875d2d394f22a3b43bfb36ec22ee7452
Peter Gledhill <petergledhill@gmail.com>
2017-09-15 13:16:02 +0100

    added swagger

--- dotnet/src/Api/Startup.cs
+++ dotnet/src/Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Chaffinch.Api
 {
@@ -24,6 +25,11 @@ namespace Chaffinch.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Chaffinch API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +39,14 @@ namespace Chaffinch.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chaffinch API V1");
+            });
 
             app.UseMvc();
         }
