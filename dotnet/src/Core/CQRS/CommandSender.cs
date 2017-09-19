4da55df8d4145badf1d0933d4ab89a0868b6e6fc
Peter Gledhill <petergledhill@gmail.com>
2017-09-19 12:05:33 +0100

    added wrapper for cqrslite commandsender

---
+++ dotnet/src/Core/CQRS/CommandSender.cs
@@ -0,0 +1,34 @@
+﻿using CQRSlite.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chaffinch.Core.CQRS
+{
+    /// <summary>
+    /// A wrapper for CQRSLites CommandSender, so as not require CQRSLite in other libraries
+    /// </summary>
+    public class CommandSender 
+    {
+        private readonly CQRSlite.Commands.ICommandSender _cqrsLiteCommandSender;
+
+        public CommandSender(CQRSlite.Commands.ICommandSender cqrsLiteCommandSender)
+        {
+            _cqrsLiteCommandSender = cqrsLiteCommandSender;
+        }
+
+        /// <summary>
+        /// Trigger the command so it can be processed by the relevent handler(s)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="command"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task Send<T>(T command, CancellationToken cancellationToken = default(CancellationToken)) where T : class, ICommand
+        {
+            return _cqrsLiteCommandSender.Send<T>(command, cancellationToken);
+        }
+    }
+}
---
+++ dotnet/tests/Core.Unit/CQRS/CommandSenderTests.cs
@@ -0,0 +1,28 @@
+﻿using Chaffinch.Core.CQRS;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chaffinch.Core.Unit.CQRS
+{
+    public class CommandSenderTests
+    {
+        [Fact]
+        public async Task test_send_command_wraps_cqrs_lite()
+        {          
+            var cqrsLiteCommandSender = new Mock<CQRSlite.Commands.ICommandSender>();
+            var commandSender = new CommandSender(cqrsLiteCommandSender.Object);
+            
+            var mockCommand = new Mock<CQRSlite.Commands.ICommand>();
+            var cancelationToken = default(CancellationToken);
+
+            await commandSender.Send(mockCommand.Object, cancelationToken);
+
+            cqrsLiteCommandSender.Verify(m => m.Send(mockCommand.Object, cancelationToken));
+        }
+    }
+}
