cd2861f757004c0dd60b51044df229bdd05cc2aa
Peter Gledhill <petergledhill@gmail.com>
2017-09-28 12:21:16 +0100

    added mongodb event store

---
+++ dotnet/src/CQRS/Events/MongoDBEventStore.cs
@@ -0,0 +1,44 @@
+﻿using CQRSlite.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using MongoDB;
+using MongoDB.Driver;
+
+namespace Chaffinch.CQRS.Events
+{
+    public class MongoDBEventStore : IEventStore
+    {
+        private readonly IEventPublisher _eventPublisher;
+        private readonly IMongoDatabase _mongoDatabase;
+
+        public MongoDBEventStore(IEventPublisher eventPublisher, IMongoDatabase mongoDatabase)
+        {
+            _mongoDatabase = mongoDatabase;
+            _eventPublisher = eventPublisher;
+        }
+
+        public async Task<IEnumerable<IEvent>> Get(Guid aggregateId, int fromVersion, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var collection = _mongoDatabase.GetCollection<IEvent>("events", null);
+            
+            await collection.FindAsync(e => e.Id == aggregateId, null, cancellationToken);
+
+            return new List<IEvent>();
+        }
+
+        public async Task Save(IEnumerable<IEvent> events, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var collection = _mongoDatabase.GetCollection<IEvent>("events");
+           
+            foreach (var @event in events)
+            {
+                await collection.InsertOneAsync(@event, null, cancellationToken);
+                await _eventPublisher.Publish(@event, cancellationToken);
+            }
+        }
+    }
+}
---
+++ dotnet/tests/CQRS.Unit/Events/MongoDBEventStoreTests.cs
@@ -0,0 +1,79 @@
+﻿using Chaffinch.CQRS.Events;
+using Chaffinch.CQRS.Unit.Mocks;
+using CQRSlite.Events;
+using MongoDB.Driver;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chaffinch.CQRS.Unit.Events
+{
+    public class MongoDBEventStoreTests
+    {
+        private readonly Mock<IEventPublisher> _publisher;
+        private readonly Mock<IMongoDatabase> _mongoDb;
+        private readonly Mock<IMongoCollection<IEvent>> _mongoCollection;
+
+        private readonly MongoDBEventStore _mongoDBEventStore;
+
+        public MongoDBEventStoreTests()
+        {
+            _publisher = new Mock<IEventPublisher>();
+            _mongoDb = new Mock<IMongoDatabase>();
+            _mongoCollection = new Mock<IMongoCollection<IEvent>>();
+                        
+            _mongoDBEventStore = new MongoDBEventStore(_publisher.Object, _mongoDb.Object);
+        }
+
+        [Fact]
+        public async Task Save_should_publish_the_provided_events()
+        {
+            var event1 = new MockEvent { Id = Guid.NewGuid() };
+            var event2 = new MockEvent { Id = Guid.NewGuid() };
+            var cancellationToken = default(CancellationToken);
+
+            await _mongoDBEventStore.Save(new List<IEvent> { event1, event2 }, cancellationToken);
+
+            _publisher.Verify(m => m.Publish<IEvent>(event1, cancellationToken));
+            _publisher.Verify(m => m.Publish<IEvent>(event2, cancellationToken));
+        }
+
+        [Fact]
+        public async Task Save_persists_events()
+        {
+            var event1 = new MockEvent { Id = Guid.NewGuid() };
+            var event2 = new MockEvent { Id = Guid.NewGuid() };
+            var cancellationToken = default(CancellationToken);
+
+            await _mongoDBEventStore.Save(new List<IEvent> { event1, event2 }, cancellationToken);
+
+            _mongoCollection.Verify(c => c.InsertOneAsync(event1, null, cancellationToken));
+            _mongoCollection.Verify(c => c.InsertOneAsync(event2, null, cancellationToken));
+        }
+
+        /*
+         * I am unable to mock the mongo find extension methods so going to have to test get  
+         * with an integration test
+         * 
+        [Fact]
+        public async Task Get_querys_mongo_for_the_events_by_aggregrate_id()
+        {
+            var aggregateId = Guid.NewGuid();        
+            var cancellationToken = default(CancellationToken);
+            Expression<Func<IEvent, bool>> findById = e => e.Id == aggregateId;
+            var eventCursor = new Mock<IAsyncCursor<IEvent>>();
+            
+            _mongoCollection.Setup(c => c.FindAsync(findById, null, cancellationToken))
+                .ReturnsAsync(eventCursor.Object);
+            
+            await _mongoDBEventStore.Get(aggregateId, 0, cancellationToken);
+            
+            _mongoCollection.Verify(c => c.FindAsync<IEvent>(findById, null, cancellationToken));            
+        }*/
+    }
+}
